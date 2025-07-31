748d9312fd3fd76460b0562a799d8d8a3ab7d2d2
a96363877 <a96363877@gmail.com>
2025-07-31 15:09:23 +0300

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,13 +2,14 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-    apiKey: "AIzaSyD1ZuKXPtM5nX0l2NEbPXAjMYDVosSh8gc",
-  authDomain: "dsfsdf-f3d65.firebaseapp.com",
-  projectId: "dsfsdf-f3d65",
-  storageBucket: "dsfsdf-f3d65.firebasestorage.app",
-  messagingSenderId: "1035037204387",
-  appId: "1:1035037204387:web:967f0b58953a999946c416",
-  measurementId: "G-SWQHF8SD4J"
+   apiKey: "AIzaSyBdEabfH-ffmrWH4uq3YVwJtlcAnWARiEo",
+  authDomain: "xczxczxczxc-9fe7d.firebaseapp.com",
+  databaseURL: "https://xczxczxczxc-9fe7d-default-rtdb.firebaseio.com",
+  projectId: "xczxczxczxc-9fe7d",
+  storageBucket: "xczxczxczxc-9fe7d.firebasestorage.app",
+  messagingSenderId: "300165970707",
+  appId: "1:300165970707:web:91e75bb4fee8c82bb9f8e6",
+  measurementId: "G-YBMR5X7T4D"
 };
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
